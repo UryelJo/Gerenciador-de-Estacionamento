41cc2d7277ae591d2034bbe0d69881b2c7f19927
uryel <uryeljodelucca18@gmail.com>
2024-10-18 09:44:02 -0300

    refactor: refatorar os botoes da pagina inicial

--- FrontEnd/GerenciadorDeEstacionamento/src/App.vue
+++ FrontEnd/GerenciadorDeEstacionamento/src/App.vue
@@ -3,7 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 import {ref } from 'vue';
 import Button from 'primevue/button';
 import Drawer from 'primevue/drawer';
-import AutoComplete from 'primevue/autocomplete';
 
 const barraDePesquisaAtiva = ref<boolean>(false);
 const visivel = ref<boolean>(true);
@@ -11,27 +10,29 @@ const visivel = ref<boolean>(true);
 
 <template>
   <div id="app">
-    <Drawer v-model:visible="visivel" header="MENU">
-      <router-link to="/">
-        <div class="logo" @click="barraDePesquisaAtiva = false">
-          <h1>VAIDERÉ.so</h1>
-          <p>Nunca vá de Frente</p>
+    <Drawer v-model:visible="visivel" class="sidebar">
+        <template #header>
+          <router-link to="/">
+            <div class="logo" @click="barraDePesquisaAtiva = false">
+              <h1>VAIDERÉ.so</h1>
+              <p>Nunca vá de Frente</p>
+            </div>
+          </router-link>
+        </template>
+        <div class="menu">
+          <RouterLink to="/gerenciar-carros">
+            <Button label="GERENCIAR CARROS" icon="bi bi-car-front-fill" iconPos ="left" text raised severity="secondary" @click = "barraDePesquisaAtiva = true"/>
+          </RouterLink>
+          <RouterLink to="/gerenciar-funcionarios">
+            <Button label="GERENCIAR FUNCIONÁRIOS" icon="bi bi-people-fill" iconPos ="left" text raised severity="secondary" @click = "barraDePesquisaAtiva = true"/>
+          </RouterLink>
+          <RouterLink to="/gerenciar-planos">
+            <Button label="GERENCIAR PLANOS" icon="bi bi-list-check" iconPos ="left" text raised severity="secondary" @click = "barraDePesquisaAtiva = true"/>
+          </RouterLink>
+          <RouterLink to="/gerenciar-estacionamento">
+            <Button label="GERENCIAR ESTACIONAMENTO" icon="bi bi-p-square-fill" iconPos ="left" text raised severity="secondary" @click = "barraDePesquisaAtiva = true"/>
+          </RouterLink>
         </div>
-      </router-link>
-      <div class="menu">
-        <RouterLink to="/gerenciar-carros">
-          <Button label="GERENCIAR CARROS" icon="bi bi-car-front-fill" iconPos ="left" @click = "barraDePesquisaAtiva = true"/>
-        </RouterLink>
-        <RouterLink to="/gerenciar-funcionarios" active-class="active">
-          <Button label="GERENCIAR FUNCIONÁRIOS" icon="bi bi-people-fill" iconPos ="left" @click = "barraDePesquisaAtiva = true"/>
-        </RouterLink>
-        <RouterLink to="/gerenciar-planos">
-          <Button label="GERENCIAR PLANOS" icon="bi bi-list-check" iconPos ="left" @click = "barraDePesquisaAtiva = true"/>
-        </RouterLink>
-        <RouterLink to="/gerenciar-estacionamento">
-          <Button label="GERENCIAR ESTACIONAMENTO" icon="bi bi-p-square-fill" iconPos ="left" @click = "barraDePesquisaAtiva = true"/>
-        </RouterLink>
-      </div>
     </Drawer>
     <Button icon="bi bi-list" @click="visivel = !visivel" style="position: absolute; top: 10px; left: 10px;"/>
 
@@ -42,7 +43,6 @@ const visivel = ref<boolean>(true);
     </main>
   </div>
 </template>
-
 <style scoped>
 
 #app {
@@ -51,44 +51,14 @@ const visivel = ref<boolean>(true);
   width: 100%;
 }
 
-.sidebar {
-  width: 305px;
-  height: 100vh;
-  background-color: #282828;
-  padding: 20px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: 3px 0px 3px 0px #00000025;
-  transition: 0.6s;
-}
-
-.sidebar:hover {
-  box-shadow: 1px 0px 50px 0px #ffffff10;
+.p-drawer{
+  background-color: rgb(0, 0, 0) !important;
 }
 
 .logo {
   text-align: center;
-  margin-bottom: 20px;
   color: rgb(255, 255, 255);
-  border-radius: 5px;
-  padding: 20px 60px 20px 60px;
   transition: 0.2s;
-}
-
-.input{
-  width: 100%;
-  padding: 10px;
-  font-size: 16px;
-  cursor: pointer;
-  border-radius: 10px;
-  border: 1px solid #ffffff00;
-  transition: 0.2s;
-}
-
-.input:focus{
-  outline: none;
 }
 
 p{
@@ -115,8 +85,7 @@ h1{
 .menu {
   display: flex;
   flex-direction: column;
-  gap: 10px;
-  width: 100%;
+  gap: 7px;
 }
 
 .main-content {
@@ -124,30 +93,6 @@ h1{
   flex: 1;
   display: flex;
   flex-direction: column;
-}
-
-.barra-de-pesquisa{
-  font-size: 20px;
-  color: rgb(255, 255, 255);
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  padding: 10px;
-  border: 1px solid #ffffff00;
-  border-radius: 10px;
-  justify-content: space-between;
-  background-color: #282828;
-  cursor: pointer;
-  transition: 0.2s;
-}
-
-.barra-de-pesquisa:hover{
-  transition: 0.2s;
-  background-color: #804bc5;
-    box-shadow: inset 3px 3px 3px 0px #ffffff25,
-                inset -3px -3px 3px 0px #00000025;
-    border: 1px solid #ffffff25;
-  
 }
 
 .content{
@@ -164,11 +109,5 @@ a:hover{
   background-color: transparent;
 }
 
-header {
-  display: flex;
-  justify-content: center;
-  padding-top: 10px;
-  margin-right: 10px
-}
 
 </style>
